63b2b8ba8b5b54134ebae1071ccb9af0c66817b7
sanjay shetty <sanjay.shetty.mys@gmail.com>
2024-04-16 13:29:50 +0530

    f

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom';
 // import './index.css';
 import App from './src/App';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
--- src/root/index.js
+++ src/root/index.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom';  // Only import createRoot once
 import './index.css';
 import App from './App';
 
-const root = createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
